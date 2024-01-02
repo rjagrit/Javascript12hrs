c5dccbd726e502a04200ceb041243faacffcfdf2
Jagrit <rjagrit.2k@gmail.com>
2024-01-02 12:02:41 +0530

    first

---
+++ firstJsPGM.js
@@ -0,0 +1,8 @@
+// for printing something on screen we use console. 
+// console.log("First JS Programin");
+// console.log("First JS Programing Language");
+
+// for stroing data we need a container called as variables
+fullname= "Jai Stark"
+
+console.log(fullname);
