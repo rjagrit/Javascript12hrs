e837b1aaf6ca98de4deaf49930455861a8e23b17
Jagrit <rjagrit.2k@gmail.com>
2024-01-02 14:06:04 +0530

    first

--- firstJsPGM.js
+++ firstJsPGM.js
@@ -1,6 +1,21 @@
+/*
+1. Javascript is a Dynamic Programming Lang. means we need not define 
+the types of data that we are stoing in the variable. 
+
+2. writing variables cases 
+fullName -> CamelCase
+fullname -> not right
+full-name -> kebab case
+FullName -> Pascal Case
+
+3. We have to use keyword with variable like 
+let, var, const 
+let and const are the more favourable keywords used in Javascript
+
+*/
 // for printing something on screen we use console. 
-// console.log("First JS Programin");
-// console.log("First JS Programing Language");
+console.log("First JS Programin");
+console.log("First JS Programing Language");
 
 // for stroing data we need a container called as variables
 fullname= "Jai Stark"
@@ -9,3 +24,29 @@ price= 99.99;
 a= null;
 y= undefined;
 console.log(fullname);
+
+// assignemnt operator let say value 201 stored in y varibale
+y= 201;
+console.log(y);
+
+//---------Variables rules-----------
+// Variable name are case sensitive; both have different meaning 
+ambinum= 21;
+Ambinum= 25;
+
+// only letters, digits, underscore and $ is allowed but space not allowed
+aaaa= 12; // v
+//12dghj= 23;// inv cuz not start wih immediate digit
+
+
+
+// only a letter, underscore(_) or $ should be 1st character.
+aaaa;
+_sss;
+$av=n; 
+
+//reserved wods cannot be variable name
+console=23;
+console.log(console);// wrong
+
+// ----------------------------------------
