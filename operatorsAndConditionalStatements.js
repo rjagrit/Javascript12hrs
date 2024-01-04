dabb9c736d4edaa5db3cac3547d993ea7e1f981f
Jagrit <rjagrit.2k@gmail.com>
2024-01-04 17:07:22 +0530

    third

--- operatorsAndConditionalStatements.js
+++ operatorsAndConditionalStatements.js
@@ -7,10 +7,35 @@
 // 1. Arithmetic Operators (Modulus, Exponential, Increment, Decrement)
 
 let a= 5, b=2;
-
+// =======Works on 2 operands============
 console.log("a =",a,"b =",b);
 console.log("a+b =",a + b);
 console.log("a-b =",a - b);
 console.log("a/b =",a / b);
 console.log("a*b =",a * b);
+
+console.log("a%b =",a % b);
+console.log("a**b =",a ** b);
+
+// =======Works on 2 operands============
+
+// =======Works on single operand============
+//unary operators
+console.log("Old value of a = ",a);
+a++ //post increment (changes value after use)
+console.log("New value of a = ",a);
+
+console.log("Old value of b = ",b);
+--b //pre decrement (changes value before use)
+console.log("New value of b = ",b);
+// ==========================================
+
+//2. Assignment Operators
+let c= 8;
+c+=1; //all arithemetic operators are allowed in assignment operators
+console.log(c);
+
+//3. Comparison Operators
+
+
 
