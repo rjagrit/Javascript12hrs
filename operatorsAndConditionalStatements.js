da13b9a9ac3f1e87c3f7c09ac64e2df8a94e5929
Jagrit <rjagrit.2k@gmail.com>
2024-01-04 16:18:57 +0530

    third

--- operatorsAndConditionalStatements.js
+++ operatorsAndConditionalStatements.js
@@ -1,2 +1,16 @@
+//Comments - Part of the code which is not executed
 //single comment 
 /* Multiline comment */
+
+// -----------Operators-----------------------------------------
+// Used to perform some operation on data
+// 1. Arithmetic Operators (Modulus, Exponential, Increment, Decrement)
+
+let a= 5, b=2;
+
+console.log("a =",a,"b =",b);
+console.log("a+b =",a + b);
+console.log("a-b =",a - b);
+console.log("a/b =",a / b);
+console.log("a*b =",a * b);
+
