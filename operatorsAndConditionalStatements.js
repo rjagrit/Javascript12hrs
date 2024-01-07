5222adc246160adfb1edbb482c5a5eb11da7eeb4
Jagrit <rjagrit.2k@gmail.com>
2024-01-07 10:42:08 +0530

    third

--- operatorsAndConditionalStatements.js
+++ operatorsAndConditionalStatements.js
@@ -37,5 +37,55 @@ console.log(c);
 
 //3. Comparison Operators
 
+let g= 5, f=2;
+let y= "5";
+console.log("a==b",a==b); // equal to
+console.log("a!=b",a!=b); // not equal to
 
+console.log("g==y",g==y); // it is true because ek string me jab ek number hota h toh javascript implicitly automatically convert number to string 
 
+console.log("g>=f",g>=f); // greater than or equal to
+console.log("g<=f",g<=f); // less than or equal to
+
+console.log("g===y",g===y); // strict version, asnswer is false here
+//4. Logial Operators (Give answer after evaluationg multiple expressions)
+
+let condition1= a>b;
+let condition2= a<b;
+
+console.log("condition1 && condition2", condition1 && condition2); //logical AND
+
+console.log("condition1 || condition2", condition1 || condition2); //logical OR
+
+console.log("condition1 != condition2", condition1!=condition2); //logical NOT
+
+// ----------------------------------------------------------------------------------
+
+//---------------Conditional Statements----------------------
+
+// 1. if statement
+let age = 25;
+
+if(age>18){
+    console.log("You can vote");
+}
+
+//2. if else statement
+let mode="dark mode";
+if(mode=="dark mode"){
+    console.log("Correct color");
+}
+else{
+    console.log("incorect color");
+}
+
+//Q. Checking the number is even or odd in JS
+
+let num= 5;
+
+if(num%2===0){
+    console.log("Number",num,"is Even");
+}
+else{
+    console.log("Number",num,"is Odd")
+}
