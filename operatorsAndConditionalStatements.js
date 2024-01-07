9499b41eb8c377f856d71ea6dc56abf25f7c9f52
Jagrit <rjagrit.2k@gmail.com>
2024-01-07 18:18:47 +0530

    third

--- operatorsAndConditionalStatements.js
+++ operatorsAndConditionalStatements.js
@@ -59,6 +59,12 @@ console.log("condition1 || condition2", condition1 || condition2); //logical OR
 
 console.log("condition1 != condition2", condition1!=condition2); //logical NOT
 
+
+// 5. Ternary Operators
+// condition? true Output: false Output
+ let aging= 14;
+ aging > 18 ? console.log("Adult") : console.log("Not Adult");
+
 // ----------------------------------------------------------------------------------
 
 //---------------Conditional Statements----------------------
@@ -89,3 +95,56 @@ if(num%2===0){
 else{
     console.log("Number",num,"is Odd")
 }
+
+
+//3.else if statement
+
+if(age<18){
+    console.log("Junior")
+}
+else if(age>60){
+console.log("Senior")
+}
+else{
+console.log("middle")
+}
+
+//MDN Docs
+
+// alert("Hello");  //one time pop message
+// prompt("Give me your name"); //prompt will give us msg and also we are able to take input
+
+// let name1= prompt("What is your Name");
+// console.log("User Name is",name1);
+
+/*-------------------------------------
+Q. Get user to input a number using prompt("Enter a number:"). Check if the number is a multiple of 5 or not
+ let number1 = prompt("Enter the Number to check if number is multiple of 5 or not");
+if(number1%5===0){
+    console.log("Number",number1,"is Multiple of 5");
+}
+else{
+    console.log("Number",number1,"is not a multiple of 5")
+}
+-----------------------------------------*/
+
+//Q. Write a code which can give grades to students accordings to their scores.
+let k;
+let marks = prompt("Enter the marks");
+if(marks>=80 && marks<=100){
+    alert("Grade is A");
+}
+else if(marks>=70 && marks<=89){
+    alert("Grade is B");
+}
+else if(marks>=60 && marks<=69){
+    alert("Grade is C");
+}
+else if(marks>=50 && marks<=59){
+    alert("Grade is D");
+}
+else
+{
+    alert("Grade is F");
+}
+
