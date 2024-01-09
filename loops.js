82812ddefe9ba897e634084e9810e1adf3caa742
Jagrit <rjagrit.2k@gmail.com>
2024-01-09 23:11:15 +0530

    fourth

---
+++ loops.js
@@ -0,0 +1,20 @@
+//-----------Wrong Method--------------
+// for(int i=0; i<=3;i++)
+// {
+//     console.log("Hi, My name is Jagrit");
+// }
+//-------------------------------------
+
+for(let i=0; i<=1;i++)
+{
+    console.log("Hi, My name is Jagrit");
+}
+
+//Q. Calculate sum of first n natural numbers
+
+let sum=0;
+for(let i=0;i<6;i++){
+    sum= sum+i;
+}
+console.log(sum);
+console.log("Loop has ended");
